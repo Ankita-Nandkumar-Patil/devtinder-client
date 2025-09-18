950c1976e9a4341cd2cb3cd328f2a73c48d5e61e
Ankita Nandkumar Patil <ankitanp31@gmail.com>
2025-09-18 12:50:15 +0530

    daisyui and tailwind config

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-blue-500">
+        Hello Tailwind!
+      </h1>
+      <button className="btn btn-primary">DaisyUI Button</button>
+    </>
+  );
+}
+
+export default App
