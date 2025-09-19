cb8a50eb697f044348ca26ee382d56c1d986476b
Ankita Nandkumar Patil <ankitanp31@gmail.com>
2025-09-19 12:26:49 +0530

    configured protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,34 @@ import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Body from "./Body";
 import Profile from "./_components/Profile";
 import Login from "./_components/Login";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
+import ProtectedRoutes from "./_components/ProtectedRoutes";
 
 function App() {
 
   return (
     <>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Body />}>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/profile" element={<Profile/>}/>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            {/* public routes */}
+            <Route path="/login" element={<Login />} />
+
+            {/* protected routes: protecting entire route group */}
+            <Route
+              path="/"
+              element={
+                <ProtectedRoutes>
+                  <Body />
+                </ProtectedRoutes>
+              }
+            >
+              <Route path="/profile" element={<Profile />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
