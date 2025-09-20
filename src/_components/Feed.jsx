ea71fbf3084c68f1a2968d5929155beb0ed1c3e1
Ankita Nandkumar Patil <ankitanp31@gmail.com>
2025-09-20 19:08:29 +0530

    updated user card, and integrated data

--- src/_components/Feed.jsx
+++ src/_components/Feed.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { BASE_URL } from "../constants";
+import UserCard from './UserCard';
+import { useDispatch, useSelector } from 'react-redux';
+import { addFeedData } from "../utils/feedSlice"
 
 export default function Feed() {
-  return (
-    <div>Feed</div>
+  const dispatch = useDispatch()
+  const feedData = useSelector((store)=> store.feed)
+
+  const getFeed = async () => {
+    if (feedData) return;
+    try {
+      const res = await axios.get(BASE_URL + "/user/feed", { withCredentials: true });
+      dispatch(addFeedData(res.data))
+    }
+    catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    getFeed()
+  },[])
+  return feedData && (
+    <div className="mt-10">
+      {/* {feedData?.data?.map} */}
+      <UserCard data={feedData?.data[0]} />
+    </div>
   )
 }
