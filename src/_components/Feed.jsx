df84426bcbf76d3f0652c5557bc621a90728acb6
Ankita Nandkumar Patil <ankitanp31@gmail.com>
2025-09-21 18:06:54 +0530

    fixed feed data removal logic

--- src/_components/Feed.jsx
+++ src/_components/Feed.jsx
@@ -1,32 +1,53 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
+import axios from "axios";
+import React, { useEffect } from "react";
 import { BASE_URL } from "../constants";
-import UserCard from './UserCard';
-import { useDispatch, useSelector } from 'react-redux';
-import { addFeedData } from "../utils/feedSlice"
+import UserCard from "./UserCard";
+import { useDispatch, useSelector } from "react-redux";
+import { addFeedData } from "../utils/feedSlice";
+import NodataSlate from "./NodataSlate"; // assuming you have this
 
 export default function Feed() {
-  const dispatch = useDispatch()
-  const feedData = useSelector((store)=> store.feed)
+  const dispatch = useDispatch();
+  const feedData = useSelector((store) => store.feed); // now an array
 
   const getFeed = async () => {
-    if (feedData) return;
+    if (feedData.length > 0) return; // skip if already loaded
     try {
-      const res = await axios.get(BASE_URL + "/user/feed", { withCredentials: true });
-      dispatch(addFeedData(res.data))
+      const res = await axios.get(BASE_URL + "/user/feed", {
+        withCredentials: true,
+      });
+      dispatch(addFeedData(res.data.data)); // only array into store
+    } catch (error) {
+      console.error(error);
     }
-    catch (error) {
-      console.error(error)
-    }
-  }
+  };
 
   useEffect(() => {
-    getFeed()
-  },[])
-  return feedData && (
-    <div className="mt-10">
-      {/* {feedData?.data?.map} */}
-      <UserCard data={feedData?.data[0]} mode="feed" />
+    getFeed();
+  }, []);
+
+  console.log("feeee", feedData);
+
+  return (
+    <div className="mt-10 flex justify-center relative">
+      {feedData.length > 0 ? (
+        feedData.map((user, index) => (
+          <div
+            key={user._id}
+            className="absolute transition-transform duration-300"
+            style={{
+              transform: `translateY(${index * 10}px) scale(${
+                1 - index * 0.05
+              })`,
+              zIndex: feedData.length - index,
+            }}
+          >
+            <UserCard data={user} mode="feed" />
+          </div>
+        ))
+      ) : (
+        <NodataSlate text="No more profiles left for now!" />
+      )}
     </div>
-  )
+  );
 }
